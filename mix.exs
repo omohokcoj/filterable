3e8e0a83eb38ce91de8eddc1ada14a37cbb7ab41
omohokcoj <xjokerua@gmail.com>
2016-09-17 15:38:40 +0300

    add mix package attributes

--- mix.exs
+++ mix.exs
@@ -5,28 +5,41 @@ defmodule Filterable.Mixfile do
     [app: :filterable,
      version: "0.0.1",
      elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+     description: description,
+     package: package,
+     deps: deps,
+     docs: &docs/0
+   ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, "~> 0.11", only: :dev}]
+  end
+
+  defp description do
+    """
+    Simple query params filtering for Phoenix framework.
+    """
+  end
+
+  defp package do
+    [name: :filterable,
+     files: ["lib", "mix.exs", "README*", "config"],
+     contributors: ["Pete Matsyburka"],
+     maintainers: ["Pete Matsyburka"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/omohokcoj/filterable",
+              "Docs" => "https://hexdocs.pm/filterable"}]
+  end
+
+  defp docs do
+    {ref, 0} = System.cmd("git", ["rev-parse", "--verify", "--quiet", "HEAD"])
+    [source_ref: ref,
+     main: "readme",
+     extras: ["README.md"]]
   end
 end
